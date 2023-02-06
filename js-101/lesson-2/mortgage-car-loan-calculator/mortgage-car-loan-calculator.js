b0a0d331c03f0c825a4d54c5f5e49e9f2c09d54e
Alex Theocharis <theocharis16@gmail.com>
2023-02-06 09:25:52 +0000

    (2) Update mortgage / car loan calculator

--- js-101/lesson-2/mortgage-car-loan-calculator/mortgage-car-loan-calculator.js
+++ js-101/lesson-2/mortgage-car-loan-calculator/mortgage-car-loan-calculator.js
@@ -6,42 +6,65 @@ let readline = require('readline-sync');
 
 let prompt = message => console.log(`=> ${message}`);
 
-prompt("Enter loan amount:");
-let loanAmount = Number(readline.question());
+let isInvalid = number => {
+  return number.trim('') === '' ||
+         Number(number) < 0     ||
+         Number.isNaN(Number(number));
+};
 
-while (loanAmount < 0 ) {
-  prompt("Enter an amount that is more than 0:");
-  loanAmount = Number(readline.question());
+prompt("Welcome to calculator!");
+
+while (true) {
+  prompt("Enter loan amount:");
+  let loanAmount = readline.question();
+
+  while (isInvalid(loanAmount)) {
+    prompt("Enter a positive amount:");
+    loanAmount = readline.question();
+  }
+
+  prompt("Enter the APR:");
+  let apr = readline.question();
+
+  while (isInvalid(apr)) {
+    prompt("Enter a positive interest rate.");
+    apr = readline.question();
+  }
+
+  prompt("Loan term (years):");
+  let loanDurationInYears = readline.question();
+
+  while (isInvalid(loanDurationInYears)) {
+    prompt("Enter a positive number.");
+    loanDurationInYears = readline.question();
+  }
+
+  prompt("Loan term (months):");
+  let loanDurationInMonths = readline.question();
+
+  while (isInvalid(loanDurationInMonths)) {
+    prompt("Enter a number positive number.");
+    loanDurationInMonths = readline.question();
+  }
+
+  let monthlyInterest = (Number(apr) / 100) / 12;
+  let finalDurationInMonths = (Number(loanDurationInYears) * 12)
+                              + Number(loanDurationInMonths);
+
+  let monthlyPayment = Number(loanAmount) * (monthlyInterest /
+  (1 - Math.pow((1 + monthlyInterest), (-finalDurationInMonths))));
+
+  prompt(`Monthly payment: $${monthlyPayment.toFixed(2)}`);
+
+  prompt("Would you like to calculate another monthly payment? (y/n)");
+  let answer = readline.question().toLowerCase();
+
+  while ((answer[0] !== 'y') && (answer[0] !== 'n')) {
+    prompt("Please enter 'y' or 'n'.");
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] === 'n') {
+    break;
+  }
 }
-
-prompt("Enter the APR:");
-let apr = Number(readline.question());
-
-while (apr < 0 ) {
-  prompt("Enter an interest rate that is more than 0% .");
-  apr = Number(readline.question());
-}
-
-prompt("Loan term (years):");
-let loanDurationInYears = Number(readline.question());
-
-while (loanDurationInYears < 0 ) {
-  prompt("Enter a number that is more than 0.");
-  loanDurationInYears = Number(readline.question());
-}
-
-prompt("Loan term (months):");
-let loanDurationInMonths = Number(readline.question());
-
-while (loanDurationInMonths > 12 || loanDurationInMonths < 0) {
-  prompt("Enter a number betweem 0 and 12 months.");
-  loanDurationInMonths = Number(readline.question());
-}
-
-let monthlyInterest = (apr / 100) / 12;
-let finalDurationInMonths = (loanDurationInYears * 12) + loanDurationInMonths;
-
-let monthlyPayment = loanAmount * (monthlyInterest /
-(1 - Math.pow((1 + monthlyInterest), (-finalDurationInMonths))));
-
-prompt(`Monthly payment: $${monthlyPayment.toFixed(2)}`);
