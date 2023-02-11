0e0079d9b599ce35d8ef9bd49f3bbed05c90c231
Alex Theocharis <theocharis16@gmail.com>
2023-02-11 16:55:14 +0000

    Adding bonus features to RPS game

--- js-101/lesson-2/rock-paper-scissors/rock_paper_scissors.js
+++ js-101/lesson-2/rock-paper-scissors/rock_paper_scissors.js
@@ -1,25 +1,34 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
 
 let prompt = message => console.log(`=> ${message}`);
+
+let playerWins = (choice, computerChoice) => {
+  return WINNING_COMBOS[choice].includes(computerChoice);
+};
 
 let displayWinner = (choice, computerChoice) => {
   prompt(`You chose ${choice}, computer chose ${computerChoice}.`);
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
+  if  (playerWins(choice, computerChoice)) {
     return 'You win!';
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-            (choice === 'paper' && computerChoice === 'scissors') ||
-            (choice === 'scissors' && computerChoice === 'rock')) {
-    return 'Computer wins!';
+  } else if (choice === computerChoice) {
+    return "It's a tie!";
   } else {
-    return "It's a tie";
+    return "Computer wins!";
   }
 };
 
 let condition = true;
+let playerWinsCounter = 0;
+let computerWinsCounter = 0;
 
 while (condition) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}.`);
@@ -35,7 +44,21 @@ while (condition) {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  prompt(displayWinner(choice, computerChoice));
+  let winner = displayWinner(choice, computerChoice);
+  prompt(winner);
+
+  if (winner === 'You win!') playerWinsCounter++;
+  else if (winner === "Computer wins!") computerWinsCounter++;
+
+  if (playerWinsCounter === 3) {
+    playerWinsCounter = 0;
+    computerWinsCounter = 0;
+    prompt("You are the grand winner!");
+  } else if (computerWinsCounter === 3) {
+    playerWinsCounter = 0;
+    computerWinsCounter = 0;
+    prompt("The computer is the grand winner!");
+  }
 
   prompt("Do you want to play again (y/n)?");
   let playAgain = readline.question().toLowerCase();
